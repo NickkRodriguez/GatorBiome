1bf919cc3621ba3194c17a1f035da4973b626daf
Nick Rodriguez <60553957+NickkRodriguez@users.noreply.github.com>
2025-03-02 23:26:04 -0500

    Added L1, L2, and Elastic Net Logistic Regression models, integrated them into main.ipynb, and had to increase max_iter to ensure convergence.

---
+++ models/lr_l1_model.py
@@ -0,0 +1,14 @@
+from sklearn.linear_model import LogisticRegression
+from utils.evaluate import evaluate_model
+
+class lr_l1_model():
+    def __init__(self):
+        self.lr = LogisticRegression(penalty='l1', solver='saga', max_iter=2000, random_state=42) # max iterations change since the default solver was struggling to converge
+        
+    def train(self, X_train, y_train):
+        self.lr.fit(X_train, y_train)
+
+    def predict(self, X_test, y_test):
+        pred = self.lr.predict(X_test)
+        prob = self.lr.predict_proba(X_test)
+        return evaluate_model(y_test, pred, prob)
