7355fe1c8bd3a2750991be2f1914864ccb38930f
Nick Rodriguez <60553957+NickkRodriguez@users.noreply.github.com>
2025-03-24 02:04:08 -0400

    Refactored feature engineering pipeline, added baseline model, and improved visualizations
    - Added baseline model using identified best model with 'All Features'
    - Integrated baseline into combined results and visualizations
    - Improved line chart alignment with dissertation and redesigned heatmaps for clarity
    - Ensured metric order aligns with dissertation (AUC, Accuracy, etc.) across all relevant files
    - Removed redundant bar chart for improved focus
    - Enhanced baseline model setup with better dummy data (1000 features, 300 informative, 0 redundant) for improved performance

--- utils/evaluate.py
+++ utils/evaluate.py
@@ -3,17 +3,16 @@ from sklearn.metrics import accuracy_score, precision_score, recall_score, f1_sc
 def evaluate_model(y_true, y_pred, y_prob=None):
     metrics = []
     
+    if y_prob is not None:
+        auc_score = roc_auc_score(y_true, y_prob, multi_class="ovr", average="weighted")
+    else:
+        auc_score = None
+        
+    metrics.append(auc_score)
     metrics.append(accuracy_score(y_true, y_pred))
     #metrics.append(precision_score(y_true, y_pred, average="weighted"))
     metrics.append(precision_score(y_true, y_pred, average="weighted", zero_division=0)) # struggling with undefined values
     metrics.append(recall_score(y_true, y_pred, average="weighted"))
     metrics.append(f1_score(y_true, y_pred, average="weighted"))
-    
-    if y_prob is not None:
-        auc_score = roc_auc_score(y_true, y_prob, multi_class="ovr", average="weighted")
-    else:
-        auc_score = None
-
-    metrics.append(auc_score)
         
     return metrics
